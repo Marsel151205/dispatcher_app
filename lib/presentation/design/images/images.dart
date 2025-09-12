367cc7bddb7c5dc1f816ec361953b7bd1cf2effa
MAMambetaliev <MAMambetaliev@beeline.kg>
2025-09-12 16:43:56 +0600

    Initial commit

---
+++ lib/presentation/design/images/images.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture icMotorcycleImage = SvgPicture.asset('assets/images/ic_motorcycle.svg');
+final SvgPicture icBusImage = SvgPicture.asset('assets/images/ic_bus.svg');
+final SvgPicture icCarImage = SvgPicture.asset('assets/images/ic_car.svg');
+final SvgPicture icTruckImage = SvgPicture.asset('assets/images/ic_truck.svg');
+final SvgPicture icTukTukImage = SvgPicture.asset('assets/images/ic_tuktuk.svg');
+final SvgPicture icBoardingImage = SvgPicture.asset('assets/images/ic_boarding.svg');
+final SvgPicture icDeliveryImage = SvgPicture.asset('assets/images/ic_delivery.svg');
